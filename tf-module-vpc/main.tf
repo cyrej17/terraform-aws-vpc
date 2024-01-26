a9828f829dd7a89c220375086cba34d53b1fdf68
09016.cyril.rejas <09016.cyril.rejas@ict888.net>
2024-01-26 22:42:30 +0800

    change source

--- tf-module-vpc/main.tf
+++ tf-module-vpc/main.tf
@@ -8,7 +8,7 @@ locals {
 
 module "vpc-aws" {
   # source  = "terraform-aws-modules/vpc/aws"
-  source = "github.com/cyrej17/aws-infra.git//tf-code-aws/prod/us-east-1/vpc"
+  source  = "app.terraform.io/cyrej_yt_ops/vpc/aws"
   version = "5.5.0"
 
   for_each = local.vpc_configs_map
